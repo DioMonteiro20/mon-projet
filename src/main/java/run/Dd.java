249d5085d1a14471d6ee7d87e19aaebd2bf4bf36
Diogo Monteiro <diogomonteiro1991@gmail.com>
2017-06-13 14:35:18 +0200

    ult mod

--- src/main/java/run/Dd.java
+++ src/main/java/run/Dd.java
@@ -2,16 +2,11 @@ package run;
 
 public class Dd {
 
-<<<<<<< HEAD
-	public void okTest()
-	{
-		
+	public void okTest() {
+
 	}
-	
-=======
-	public void okMaster()
-	{
-		
+
+	public void okMaster() {
+
 	}
->>>>>>> refs/heads/master
 }
